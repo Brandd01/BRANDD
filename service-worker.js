d540910f2dd4de4e0fdeacca165f4fef4df01eff
Brandd01 <abakmesut0147@gmail.com>
2026-02-06 16:57:39 +0300

    Upgrade service worker to version 2 and add cache cleanup
    Updated cache version and added activate event to clean up old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "brandd-v1";
+const CACHE = "brandd-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,13 +8,17 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
